05360044cac725c82a191ad6bedc30d9556c7ee3
Luís Millani <luhguioliveira10@gmail.com>
2026-09-03 08:33:34 -0300

    tema claro e escuro e finalização da calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -9,3 +9,36 @@ function voltar() {
     const exibir = document.querySelector('#visor');
     exibir.value = exibir.value.slice(0, -1)
 }
+
+function resultado() {
+    const exibir = document.querySelector('#visor');
+    try {
+        exibir.value = eval(exibir.value)
+
+    } catch {
+        exibir.value = "Error"
+    }
+}
+
+//-----------------------------------------------------------
+//DarkMode//
+
+let darkmode = localStorage.getItem('darkmode');
+const botao_tema = document.getElementById('botao_tema');
+
+function abilitarDarkMode() {
+    document.documentElement.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active')
+}
+
+function desabilitarDarkMode() {
+    document.documentElement.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if (darkmode === "active") abilitarDarkMode()
+
+botao_tema.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? abilitarDarkMode() : desabilitarDarkMode()
+})
